75348bbdf65abe21c80f9bbfc4fb388cf21d380c
MyJacking <2806705978@qq.com>
2021-01-15 17:34:34 +0800

    update component contextMenu

--- src/iooc/components/index.js
+++ src/iooc/components/index.js
@@ -1,5 +1,9 @@
 import IconSvg from "./icon-svg/index.vue";
+import Avatar from "./avatar/index.vue"
+import ContextMenu from "./context-menu"
 
 export default function (app) {
     app.component("icon-svg", IconSvg);
+    app.component("io-avatar", Avatar);
+    app.component("io-context-menu", ContextMenu);
 }
